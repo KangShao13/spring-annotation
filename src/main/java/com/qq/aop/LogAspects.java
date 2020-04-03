3b4c2d9036fdf968f69cddc7abbfd7ac473c711d
kang <kang@qq.com>
2020-04-03 13:02:52 +0800

    提交分支上的修改

--- src/main/java/com/qq/aop/LogAspects.java
+++ src/main/java/com/qq/aop/LogAspects.java
@@ -30,6 +30,7 @@ public class LogAspects {
 	@Pointcut("execution(public int com.qq.aop.MathCalculator.*(..))")
 	public void pointCut(){};
 	
+	
 	//@Before在目标方法之前切入；切入点表达式（指定在哪个方法切入）
 	@Before("pointCut()")
 	public void logStart(JoinPoint joinPoint){
